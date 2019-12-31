d85594a3fa5d556d542c6907d18acd681d32d6ba
firetreey <firetreey@gmail.com>
2019-12-31 18:40:37 +0800

    [new feature] add next and antd

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from 'antd'
+
+const Index = () => (
+    <div>
+        <h1>hhh</h1>
+        <Link href='/user'>
+            <Button>asd</Button>
+        </Link>
+    </div>
+)
+
+export default Index
